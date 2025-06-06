49d108ed4427e87070f3e20a68310f35336bd526
Maykon Carvalho <carvalhomaykon566@gmail.com>
2025-06-06 00:05:52 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	// Controlador
+    @GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "91999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
